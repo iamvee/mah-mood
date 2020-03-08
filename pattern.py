88e58fc161c451929e674045acd7e5515dea0476
Vahid Næini <naeini.v@gmail.com>
2020-03-09 03:13:30 +0330

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,32 +1,59 @@
 #!/usr/bin/env python3
 import  os
 import time
+import datetime
 
 
-os.system("git init")
+coef = 20
+username = "iamvee"
+repo = "mah-mood"
+ 
+MILLISECONDS_PER_DAY = 24*3600
+PATTERN = """
+..9...................99...........................
+..9...................99...........................
+..9...................99...........................
+..9.999......9999.9...99...........................
+..99..99....99...9.................................
+..99...99...99...99...99...........................
+..99...99....9999.99..99...........................
+"""
 
-with open('pattern') as f:
-    lines = f.read().replace('.', '0').replace('#', '9').split('\n')
+
+def transpose(pattern):
+    lines = pattern.replace('.', '0').split('\n')
     arr = [[int(c) for c in line] for line in lines if line]
 
-arrx = [[arr[i][j] for i in range(len(arr))] for j in range(len(arr[0]))][::-1]
+    a, b = len(arr), len(arr[0])
+    arrx = [[arr[i][j] for i in range(a)] for j in range(b)]
+    arrx = arrx[::-1]
+    return arrx
 
-now = time.time()
 
-for i, x in enumerate(arrx):
-    for j, y in enumerate(x):
-        for yy in range(y):
-            tx = now -  24*3600 * (i*7+j) - yy
-            os.system(f"""
-echo . >> file
-git add .
-git commit -m "test $i"
-GIT_COMMITTER_DATE="`date --date=@{tx} `"
-git commit --amend --no-edit --date "`date --date=@{tx}`"
-""")
+def time_offset_gen(i, j, coef_index, weekday):
+    days = j - weekday + 1
+    return MILLISECONDS_PER_DAY * (i * 7 - days) - coef_index
+
+
+def do_commit(i, j, k, now, weekday):
+    tx = now -  time_offset_gen(i, j, k, weekday)
+    os.system(f"echo {tx} > file") 
+    os.system(f"git add . ")
+    os.system(f"git commit -m 'test $i'" )
+    os.system(f"GIT_COMMITTER_DATE='`date --date=@{tx}`'")
+    os.system(f"git commit --amend --no-edit --date '`date --date=@{tx}`'")
+
+
+if __name__ == '__main__':
+    os.system("git init")
+    now = time.time()
+    weekday = datetime.datetime.now().weekday()
+    remote = f"git@github.com:{username}/{repo}.git"
+
+    for i, x in enumerate(arrx):
+        for j, y in enumerate(x):
+            for k in range(coef * y):
+                do_commit(i, j, k, now, weekday)
             
 
-os.system("""
-git remote add origin git@github.com:iamvee/mahmood.git
-git push -u origin master
-""")    
+    os.system(f"git remote add origin {remote} && git push -u origin master")    
